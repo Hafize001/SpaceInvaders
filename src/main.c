833a1c8993ac7978d00d31712586baf060e2cb93
zümra kızıltaş <z.kiziltas77@gmail.com>
2026-04-26 03:24:07 +0300

    oyunun temel döngüsü kuruldu

--- src/main.c
+++ src/main.c
@@ -1,13 +1,54 @@
 #include "raylib.h"
+#include <stdio.h>
+
+const int screen_width = 800;
+const int screen_height = 600;
+
+// yapilar tanimlanicak(player, enemy gibi)
+
 
 int main() {
-    InitWindow(800, 600, "Space Invaders - Altyapi Hazir");
+    InitWindow(800, 600, "Space Invaders Game");
     SetTargetFPS(60);
 
+    bool paused = false;
+    bool game_over = false;
+
     while (!WindowShouldClose()) {
+
+        if (IsKeyPressed(KEY_P)) {
+            paused = !paused;
+        }
+        if (!paused && !game_over) {
+            
+            float dt = GetFrameTime();
+            
+            //dosyalarimizdaki fonksiyonlari burada cagirmaliyiz
+            
+        }
         BeginDrawing();
             ClearBackground(BLACK);
             DrawText("Raylib Calisiyor!", 300, 280, 20, RAYWHITE);
+            if (paused) {
+            // oyun durduruldugunda ekrana yazilacaklar
+            const char* pause_text = "PAUSED";
+            int text_width = MeasureText(pause_text, 20);
+            DrawText(pause_text, (screen_width - text_width) / 2, screen_height / 2, 20, YELLOW);
+        } 
+        else if (game_over) {
+            // Oyun bittiğinde ekrana yazdırılacaklar
+            const char* over_text = "GAME OVER";
+            int text_width = MeasureText(over_text, 40);
+            DrawText(over_text, (screen_width - text_width) / 2, screen_height / 2, 40, RED);
+        }else {
+            // Oyun normal akarken çizilecek nesneler
+            // DrawPlayer(&gemi);
+            // DrawBullets();
+            // DrawEnemies();
+            
+            // Şimdilik sistemin çalıştığını görmek için geçici bir yazı
+            DrawText("Oyun Motoru Aktif! Kodlar Buraya Gelecek.", 10, 10, 20, GREEN);
+        }
         EndDrawing();
     }
 
