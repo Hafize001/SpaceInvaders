58c871b0dbcdc33070865c75a55eba952d303d62
zümra kızıltaş <z.kiziltas77@gmail.com>
2026-05-03 03:03:08 +0300

    oyun ekranı arcade e gore ayarlandi

--- include/common.h
+++ include/common.h
@@ -2,11 +2,15 @@
 #define COMMON_H
 
 // --- EKRAN AYARLARI ---
-#define SCREEN_WIDTH  800
-#define SCREEN_HEIGHT 600
+#define SCREEN_WIDTH  1920
+#define SCREEN_HEIGHT 1080
 
 // --- OYUNCU (PLAYER) AYARLARI ---
 #define PLAYER_SPEED 300.0f
 #define PLAYER_LIVES 3
+
+// --- SINIRLAR ---
+#define LEFT_BOUND 420
+#define RIGHT_BOUND (1920 - 420) 
 
 #endif
