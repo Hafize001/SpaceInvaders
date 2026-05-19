2a6049ba65070f553315ce6cfadab4713dbe5265
Hafize Elmasoglu <hafizeelmasoglu0@gmail.com>
2026-05-19 23:40:45 +0300

    Revert "kullanıcı adı ve skor tablosu eklendi"
    This reverts commit ccd728efcbb18db7d671e15b8977986c36e2699a.

--- include/gamestate.h
+++ include/gamestate.h
@@ -3,8 +3,6 @@
 
 typedef enum GameScreen {
     SCREEN_MENU = 0,
-    SCREEN_NAME_INPUT,    // İsim yazma ekranı
-    SCREEN_LEADERBOARD,   // S tuşuyla açılan sıralama tablosu
     SCREEN_TRANSITION,
     SCREEN_GAMEPLAY,
     SCREEN_GAMEOVER,
